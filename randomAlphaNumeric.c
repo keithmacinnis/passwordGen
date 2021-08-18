2a9e74209a06750c43e79ceeabd68e977e8d96c5
Keith MacInnis <keithmacinnis@gmail.com>
2021-08-18 15:29:47 -0700

    Add files via upload

---
+++ randomAlphaNumeric.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void randomizer(char *s, const int len) {
+    static const char alphanum[] =
+        "0123456789!$"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) 
+        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+        
+    s[len] = 0;
+}
+
+int main(void) {
+    char *str = malloc(32 + 1);
+    srand(time(NULL));  
+    randomizer(str, 32);
+    printf("%s\n", str);
+    free(str);
+    return 0;
+}
